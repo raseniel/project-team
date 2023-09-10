5aef9330bb696b02ce126f55f52dee7f3dc6c0e2
raseniel <136839233+raseniel@users.noreply.github.com>
2023-09-10 18:46:21 +0000

    Deploying to gh-pages from @ raseniel/project-team@a1f9f5e8c765dbcc298a482f9e39b37b00750b52 🚀

--- index.b4d7b95f.js
+++
@@ -1,2 +0,0 @@
-(()=>{const t={openModalBtn:document.querySelector("[contact-data-modal-open]"),closeModalBtn:document.querySelector("[contact-data-modal-close]"),modal:document.querySelector("[contact-data-modal]")};function o(){t.modal.classList.toggle("contact-is-hidden")}t.openModalBtn.addEventListener("click",o),t.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.b4d7b95f.js.map
--- index.cbf65728.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={openModalBtn:document.querySelector("[contact-data-modal-open]"),closeModalBtn:document.querySelector("[contact-data-modal-close]"),modal:document.querySelector("[contact-data-modal]")};function o(){t.modal.classList.toggle("contact-is-hidden")}t.openModalBtn.addEventListener("click",o),t.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.cbf65728.js.map
