bca93927c001f4bb6bc1764f9c1f973635a81f11
raseniel <136839233+raseniel@users.noreply.github.com>
2023-09-13 03:25:52 +0000

    Deploying to gh-pages from @ raseniel/project-team@461462420c550cd0c5d5fb2b942a4d4e3113b26d 🚀

---
+++ index.5f395e31.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),closeNavBtn:document.querySelector("[data-nav-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n),e.closeNavBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.5f395e31.js.map
